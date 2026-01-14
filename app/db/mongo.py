c669eafbe2c8d9faf17c790222e03e2915bdb36c
Afzal <126405075+qwerty0089097@users.noreply.github.com>
2026-01-14 17:38:09 +0530

    Create mongo.py

---
+++ app/db/mongo.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import os
+
+MONGO_URI = os.getenv("MONGODB_URI")
+
+if not MONGO_URI:
+    raise RuntimeError("MONGODB_URI is not set")
+
+client = MongoClient(MONGO_URI)
+
+# database name from URI or explicit
+db = client.get_database()
+
+def get_db():
+    return db
